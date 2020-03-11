f864574f5575948337734a45296ebee363f9d44c
Blahclah <60612750+Blahclah@users.noreply.github.com>
2020-03-11 08:48:09 -0500

    Delete Lockpick.py

--- Lockpick.py
+++
@@ -1,8 +0,0 @@
-class Lockpick(object):
-
-    def __init__(self,player):
-        self.player = player
-        self.durability = 100
-
-    def pick (self):
-        durability  -= random.randint(5,10)
