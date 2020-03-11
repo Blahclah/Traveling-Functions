82366cf57a74ea4093eec95eb1998d216210a34d
Blahclah <adrianswindle@amdg.rockhusths.edu>
2020-03-11 08:46:02 -0500

    Game

---
+++ Main.py
@@ -0,0 +1,79 @@
+
+
+continent =0 
+
+def Intro():
+    print("Locations: ")
+    print("1: North America")
+    print("2: South America")
+    print("3: Europe")
+    print("4: Africa")
+    print("5: Asia")
+    print("6: Antarctic")
+        
+    continent = int(input("Where do you want to go? "))
+
+    
+    
+        
+    
+
+
+       
+def NorthAmerica():
+    print("Cities: ")
+    print("New York City")
+    print("Kansas City")
+    print("Las Angeles")
+    print("Mexico City")
+    print("Vancouver")
+    print("Toronto")
+
+    route = input(str("Which city do you want to go to? "))
+
+def SouthAmerica():
+    print("Cities: ")
+    print("Rio De Jeniro")
+    print("Benous Aires")
+    print("Lima")
+
+    route = input(str("Which city do you want to go to? "))
+
+        
+def Europe():
+    print("Cities: ")
+    print("London")
+    print("Paris")
+    print("Rome")
+
+def Africa():
+    print("Cities: ")
+    print("Cairo")
+    print("Kinshasa")
+    print("Cape Town")
+
+    route = input(str("Which city do you want to go to? "))
+
+def Asia():
+    print("Cities: ")
+    print("Hong Kong")
+    print("Tokyo")
+    print("Seoul")
+
+    route = input(str("Which city do you want to go to? "))
+
+def Antarctic():
+    i =0
+    while(i==0):
+        print("YOU DIE")
+
+
+
+Intro()
+
+if(continent == 1):
+    NorthAmerica()
+
+
+
+
